192e6f0ff1a03f6b270db166fa8e40790849c700
jram723 <avenge90.jr@gmail.com>
2021-09-01 19:47:17 -0400

    First commit

--- hello.py
+++ hello.py
@@ -2,23 +2,49 @@
 
 import subprocess
 import optparse
+import re
 
-parser = optparse.OptionParser()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC adress")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify new mac, use --help for more info.")
+    return options
 
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC adress")
 
-(options, arguments) = parser.parse_args()
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-interface = options.interface
-new_mac = options.new_mac
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
 
-# subprocess.call("ifconfig " + interface + " down",  shell=True)
-# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ifconfig " + interface + " up", shell=True)
+options = get_arguments()
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[+] Successfully changed MAC to " + current_mac)
+
+
+
+
+
