df6c50ff970b2ed455fe803f690efe79dbcf2377
jram723 <avenge90.jr@gmail.com>
2021-09-04 01:06:06 -0400

    First commit

---
+++ tcpscanner.py
@@ -0,0 +1,51 @@
+import socket
+from datetime import datetime, timedelta
+
+from IPy import IP
+
+
+def check_ip(ip):
+    try:
+        IP(ip)
+        return (ip)
+    except ValueError:
+        return socket.gethostbyname(ip)
+
+out_file = ""
+
+print("-" * 50)
+print("    Project B Scanner        ")
+print("    project By Ivan Ramirez  ")
+print("    Created Sept 1, 2021     ")
+print("-" * 50)
+
+ipaddress = input('Enter target to scan: ')
+converted_ip = check_ip(ipaddress)
+print("time started: " + str(datetime.now()))
+t1 = datetime.now()
+
+
+
+def scan_port(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
+        return '[+] Port' + str(port) + ' Is Open'
+    except:
+        return '[-] Port' + str(port) + ' Is Closed'
+
+
+
+t2 = datetime.now()
+total = t2 - t1
+print("scanning complete in: ", total )
+
+for port in range(1, 1025):
+    result = scan_port(converted_ip, port)
+    print(result, str(datetime.now()))
+    out_file = open("test.txt", "a")
+    out_file.write(str(result) + " \n " + str(total) + " \n " + str(datetime.now()))
+    out_file.close()
+
+
